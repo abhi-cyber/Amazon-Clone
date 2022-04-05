95730812337898a3e03eaec23aab5797db9b5203
abhi-cyber <abhirajchatrath@gmail.com>
2022-04-05 23:41:29 +0530

    Added Data Layer To Payment Page

--- src/Payment.js
+++ src/Payment.js
@@ -1,21 +1,44 @@
 import React from 'react';
+import CheckoutProduct from './CheckoutProduct';
 import './Payment.css';
 import { useStateValue } from './StateProvider';
 
 function Payment() {
-    
+    const [{ basket, user }, dispatch] = useStateValue();
 
     return (
         <div className='payment'>
             <div className='payment__container'>
                 {/* Payment section - delivery address */}
                 <div className='payment__section'>
-                    
+                    <div className='payment__title'>
+                        <h3>Delivery Address</h3>
+                    </div>
+                    <div className='payment__address'>
+                        <p>{user?.email}</p>
+                        <p>House No. 1595/1</p>
+                        <p>Chandigarh</p>
+                    </div>
 
                 </div>
 
                 {/* Payment section - Review Items */}
-                <div className='payment__sectioin'></div>
+                <div className='payment__sectioin'>
+                    <div className='payment__title'>
+                        <h3>Review items and delivery</h3>
+                    </div>
+                    <div className='payment__items'>
+                        {basket.map(item => (
+                            <CheckoutProduct 
+                                id={item.id}
+                                image={item.image}
+                                title={item.title}
+                                price={item.price}
+                                rating={item.rating}
+                            />
+                        ))}
+                    </div>
+                </div>
 
                 {/* Payment section - Payment Method */}
                 <div className='payment__sectioin'></div>
